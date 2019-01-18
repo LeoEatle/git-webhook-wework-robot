ae0126bcc2f5857f0c4f1c69c15546267120cd1d
LeoEatle <liuyitao811@hotmail.com>
2019-01-18 21:14:28 +0800

    style(style): update

--- src/controller/gitWebhook.ts
+++ src/controller/gitWebhook.ts
@@ -1,11 +1,22 @@
-import { BaseContext } from 'koa';
+import { BaseContext } from "koa";
 
+const HEADER_KEY = "x-event";
+
+const EVENTS = {
+    "Push Hook": "push",
+    "Tag Push Hook": "tag_push",
+    "Issue Hook": "issue",
+    "Note Hook": "note",
+    "Merge Request Hook": "merge_request",
+    "Review Hook": "review"
+};
 export default class GeneralController {
-
-    public static async getWebhook (ctx: BaseContext) {
-        console.log('git webhook req', ctx.request);
-        ctx.body = '收到了';
+    public static async getWebhook(ctx: BaseContext) {
+        console.log("git webhook req", ctx.request);
+        const body = ctx.request.body;
+        console.log("http body", body);
+        const event = ctx.request.header["x-gitlab-event"];
+        // switch (event)
+        ctx.body = "收到了";
     }
-
-
 }
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import * as Koa from 'koa';
-import * as jwt from 'koa-jwt';
-import * as bodyParser from 'koa-bodyparser';
-import * as helmet from 'koa-helmet';
-import * as cors from '@koa/cors';
-import * as winston from 'winston';
-import * as dotenv from 'dotenv';
+import * as Koa from "koa";
+import * as jwt from "koa-jwt";
+import * as bodyParser from "koa-bodyparser";
+import * as helmet from "koa-helmet";
+import * as cors from "@koa/cors";
+import * as winston from "winston";
+import * as dotenv from "dotenv";
 // import { createConnection } from 'typeorm';
 // import 'reflect-metadata';
 // import * as PostgressConnectionStringParser from 'pg-connection-string';
 
-import { logger } from './logging';
-import { config } from './config';
-import { router } from './routes';
+import { logger } from "./logging";
+import { config } from "./config";
+import { router } from "./routes";
 
 // Load environment variables from .env file, where API keys and passwords are configured
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
 const app = new Koa();
 
@@ -34,7 +34,7 @@ app.use(bodyParser());
 // app.use(jwt({ secret: config.jwtSecret }));
 
 // this routes are protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes());
 
 app.listen(config.port);
 
