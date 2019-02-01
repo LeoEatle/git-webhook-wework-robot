ce6ccbcaa05ab74c8987d6daeaf07ea4af90728c
LeoEatle <liuyitao811@hotmail.com>
2019-02-01 21:20:18 +0800

    ci(pm2): fix pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    name: 'wework-robot',
+    script: './dist/server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '134.175.32.212',
+      ref  : 'origin/master',
+      repo : 'git@github.com:LeoEatle/git-webhook-wework-robot.git',
+      path : '/root/wework-robot/git-webhook-wework-robot',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
